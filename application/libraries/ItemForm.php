525a04e9d33d3b44b52464ca826a0bfef2559ca4
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2011-03-08 04:18:03 +0000

    Acertando tab e inserido javascript e thema

--- application/libraries/ItemForm.php
+++ application/libraries/ItemForm.php
@@ -2,68 +2,68 @@
 
 class ItemForm
 {
-	private $label;
-	private $name;
-	private $id;
-	private $value;
-	private $checked;
-	private $type;
+    private $label;
+    private $name;
+    private $id;
+    private $value;
+    private $checked;
+    private $type;
 
-	private $itens;
+    private $itens;
 
-	public function __construct($label, $name, $id, $value, $checked, $type)
-	{
-		$this->label   = $label;
-		$this->name    = $name;
-		$this->id      = $id;
-		$this->value   = $value;
-		if (!empty($checked) && $checked == $this->value) {
-			$this->checked = TRUE;
-		}
-		$this->type    = $type;
-	}
+    public function __construct($label, $name, $id, $value, $checked, $type)
+    {
+        $this->label   = $label;
+        $this->name    = $name;
+        $this->id      = $id;
+        $this->value   = $value;
+        if (!empty($checked) && $checked == $this->value) {
+            $this->checked = TRUE;
+        }
+        $this->type    = $type;
+    }
 
-	public function addItem($label, $name, $id, $value, $checked = null, $type = null)
-	{
-		$this->itens[] = new ItemForm($label, $name, $id, $value, $checked, $type);
-	}
-	public function getLabel() {
-		return $this->label;
-	}
+    public function addItem($label, $name, $id, $value, $checked = null, $type = null)
+    {
+        $this->itens[] = new ItemForm($label, $name, $id, $value, $checked, $type);
+    }
+    public function getLabel() {
+        return $this->label;
+    }
 
-	public function getName(){
-		return $this->name;
-	}
+    public function getName(){
+        return $this->name;
+    }
 
-	public function getId() {
-		return $this->id;
-	}
+    public function getId() {
+        return $this->id;
+    }
 
-	public function getValue() {
-		return $this->value;
-	}
+    public function getValue() {
+        return $this->value;
+    }
 
-	public function getType() {
-		return $this->type;
-	}
+    public function getType() {
+        return $this->type;
+    }
 
-	public function getArrayAttr()
-	{
-		return array(
-			'name'  => $this->name,
-			'id'    => $this->id,
-			'value' => $this->value
-		);
-	}
+    public function getArrayAttr()
+    {
+        return array(
+            'name'  => $this->name,
+            'id'    => $this->id,
+            'value' => $this->value
+        );
+    }
 
-	public function getItens()
-	{
-		return $this->itens;
-	}
+    public function getItens()
+    {
+        return $this->itens;
+    }
 
-	public function isChecked()
-	{
-		return isset($this->checked) ? TRUE : FALSE;
-	}
+    public function isChecked()
+    {
+        return isset($this->checked) ? TRUE : FALSE;
+    }
 }
 ?>
--- application/models/pessoa.php
+++ application/models/pessoa.php
@@ -2,47 +2,52 @@
 
 class Pessoa extends CI_Model
 { 
-	public function __construct()
-	{
-		parent::__construct(); 
-	}
+    public function __construct()
+    {
+        parent::__construct(); 
+    }
 
-	public function listar()
-	{
-		$query = $this->db->get('pessoa');
+    public function listar()
+    {
+        $query = $this->db->get('pessoa');
 
-		return $query->result();
-	}
+        return $query->result();
+    }
 
-	public function inserir(array $dados)
-	{
-		$this->db->insert('pessoa', $dados);
-	}
+    public function inserir(array $dados)
+    {
+        $this->db->insert('pessoa', $dados);
+    }
 
-	public function atualizar(array $dados)
-	{
-		$id = $dados['id'];
-		unset($dados['id']);
-		$this->db->where('id', $id);
-		$this->db->update('pessoa', $dados);
-	}
+    public function atualizar(array $dados)
+    {
+        $id = $dados['id'];
+        unset($dados['id']);
+        $this->db->where('id', $id);
+        $this->db->update('pessoa', $dados);
+    }
 
-	public function getById($id)
-	{
-		$query = $this->db->get_where('pessoa', array('id' => $id));
-		$pessoa = $query->result();
-		return $pessoa[0];
-	}
+    public function getById($id)
+    {
+        $query = $this->db->get_where('pessoa', array('id' => $id));
+        $pessoa = $query->result();
+        return $pessoa[0];
+    }
 
-	public function buscar($dado)
-	{
-		$this->db->select('*')->from('pessoa')->or_like(array(
-			'nome'      => $dado,
-			'sobrenome' => $dado,
-			'sexo'      => $dado
-		));
-		$query = $this->db->get();
-		return $query->result();
-	}
+    public function buscar($dado)
+    {
+        $this->db->select('*')->from('pessoa')->or_like(array(
+            'nome'      => $dado,
+            'sobrenome' => $dado,
+            'sexo'      => $dado
+        ));
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function excluir($id)
+    {
+        $this->db->delete('pessoa', array('id' => $id));
+    }
 }
 ?>
