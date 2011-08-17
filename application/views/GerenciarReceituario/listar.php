eda832568f2aa6dc9fd43a4f400e00f2dcfe8dce
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2011-08-17 03:44:10 +0000

    Implementação para registrar receituários médicos

---
+++ application/views/GerenciarReceituario/listar.php
@@ -0,0 +1,10 @@
+<table class="layout-grid">
+<tr><?php $this->titulos->render(); ?></tr>
+<?php foreach ($receituarios as $key => $receituario): ?>
+    <tr class ="<?= ($key % 2) ? 'impar' : 'par'; ?>">
+        <td><?= $receituario->sintomas; ?></td>
+        <td><?= $receituario->data_sintomaBR; ?></td>
+        <td nowrap align="center"><?= $this->acoes->render($receituario->id); ?></td>
+    </tr>
+<?php endforeach; ?>
+</table>
